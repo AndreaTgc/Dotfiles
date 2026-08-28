9834bfb9d5c2ab11391519b407d9293cc503caf1
AndreaTgc <andreacolombo@Andreas-MacBook-Air.local>
2026-08-28 12:33:54 +0200

    rofi styling and other things

--- waybar/scripts/rofi-power.sh
+++ waybar/scripts/rofi-power.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+ROFI_THEME="$HOME/.config/rofi/powermenu-style.rasi"
+
 choice=$(printf "Logout\nSuspend\nReboot\nShutdown\n" |
-    rofi -dmenu -i -p "Power")
+    rofi -dmenu -i -p "Power" -theme="$ROFI_THEME")
 
 case "$choice" in
     "Logout") hyprctl dispatch exit ;;
