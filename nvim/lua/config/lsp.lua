d57b0420d808205d6ccd28aa4e4d5bff492f04ec
AndreaTgc <100370747+AndreaTgc@users.noreply.github.com>
2026-08-30 12:04:21 +0200

    neovim lsp and plugins config

---
+++ nvim/lua/config/lsp.lua
@@ -0,0 +1,38 @@
+vim.lsp.enable({
+    "lua_ls",
+})
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+        local map = function(mode, lhs, rhs)
+            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
+        end
+
+        map("n", "gd", vim.lsp.buf.definition)
+        map("n", "gD", vim.lsp.buf.declaration)
+        map("n", "gr", vim.lsp.buf.references)
+        map("n", "gi", vim.lsp.buf.implementation)
+        map("n", "K", vim.lsp.buf.hover)
+        map("n", "<leader>rn", vim.lsp.buf.rename)
+        map("n", "<leader>ca", vim.lsp.buf.code_action)
+        map("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end)
+        map("n", "[d", function() vim.diagnostics.jump({ count = -1, float = true }) end)
+        map("n", "]d", function() vim.diagnostics.jump({ count = 1, float = true }) end)
+        map("n", "<leader>d", vim.diagnostic.open_float)
+
+        if client and client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = false })
+        end
+    end,
+})
---
+++ nvim/lua/config/opts.lua
@@ -0,0 +1,18 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+vim.opt.undofile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.scrolloff = 10
+
